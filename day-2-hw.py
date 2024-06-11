1d9dd1b29ee63b7c68a21c8d1e128b2a28128bc0
NicolasRodriguez115 <nr115935@gmail.com>
2024-06-11 17:48:48 -0400

    first commit

---
+++ day-2-hw.py
@@ -0,0 +1,34 @@
+# Assignments: Dive Deeper
+
+# 1. Decisions at the Crossroad
+
+# number = input("Enter a number: ")
+
+# if number > 0:
+#     print("The number is positive.")
+# elif number = 0:
+#     print("The number is zero.")
+# else number < 0:
+#     print("The number is negative.")
+
+# number = input("Enter a number: ")
+
+# if int(number) > 0:
+#     print("The number is positive.")
+# elif int(number) == 0:
+#     print("The number is zero.")
+# else:
+#     int(number) < 0
+#     print("The number is negative.")
+
+# 2. The Greatest Showdown
+# Task 1: Identify the Greatest
+
+num_1, num_2, num_3 = int(input("Enter first number:\n")), int(input("Enter second number:\n")), int(input("Enter third number:\n"))
+
+if num_1 >= num_2 and num_3:
+    print(f"The largest number is {num_1} ")
+elif num_2 >= num_1 and num_3:
+    print(f"The largest number is {num_2} ")
+else:
+    print(f"The largest number is {num_3}") 
